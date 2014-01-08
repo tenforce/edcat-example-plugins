83844682d1d3978ca9bac2c8943fcd1b94d99309
nielsv <niels.vandekeybus@tenforce.com>
2014-01-08 10:18:11 +0100

    hookmanager: use java serviceloader to detect handlers available

--- src/main/java/eu/lod2/edcat/auth/AuthenticationProvider.java
+++ src/main/java/eu/lod2/edcat/auth/AuthenticationProvider.java
@@ -1,18 +1,18 @@
-import eu.lod2.edcat.hooks.util.ActionAbortException;
-import eu.lod2.edcat.hooks.constraints.Constraint;
-import eu.lod2.edcat.hooks.handlers.dcat.PreCreateHandler;
-import eu.lod2.edcat.hooks.handlers.dcat.PreReadHandler;
-import eu.lod2.edcat.hooks.handlers.dcat.PreUpdateHandler;
-import eu.lod2.edcat.hooks.constraints.Priority;
+package eu.lod2.edcat.auth;
+
 import eu.lod2.edcat.utils.SparqlEngine;
-import org.springframework.stereotype.Repository;
+import eu.lod2.hooks.constraints.Constraint;
+import eu.lod2.hooks.constraints.Priority;
+import eu.lod2.hooks.handlers.dcat.PreCreateHandler;
+import eu.lod2.hooks.handlers.dcat.PreReadHandler;
+import eu.lod2.hooks.handlers.dcat.PreUpdateHandler;
+import eu.lod2.hooks.util.ActionAbortException;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 
-@Repository
-public class Authentication implements PreCreateHandler,PreUpdateHandler,PreReadHandler {
+public class AuthenticationProvider implements PreCreateHandler,PreUpdateHandler,PreReadHandler {
 
   @Override
   public void handlePreCreate(HttpServletRequest request, SparqlEngine engine) throws ActionAbortException {
